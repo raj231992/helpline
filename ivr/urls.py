31b8cbb96ebfdac6717bbfc51344d041dba8be2d
Raj <nagarajum@cse.iitb.ac.in>
2017-03-24 22:23:16 +0530

    updated audios

--- ivr/urls.py
+++ ivr/urls.py
@@ -5,5 +5,5 @@ from .views import IVR, Feedback
 
 urlpatterns = [
     url(r'^feedback/$', Feedback.as_view(), name='getFeedback'),
-    url(r'^$', IVR.as_view(), name='getHelplines'),
+    url(r'^$', IVR.as_view(), name='getIVR'),
 ]
